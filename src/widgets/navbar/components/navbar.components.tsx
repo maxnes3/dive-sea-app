364246c5ba0a457d725b8298b31d1744195bca08
maxnes3 <draw_max@mail.ru>
2024-09-27 16:23:45 +0400

    feature: complite desktop adaptive, start menu burger

--- src/widgets/navbar/components/navbar.components.tsx
+++ src/widgets/navbar/components/navbar.components.tsx
@@ -1,5 +1,6 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import SeacrhIcon from '@shared/icons/search.svg';
+import MenuIcon from '@shared/icons/menu.svg';
 import styles from '../navbar.module.scss';
 
 type LinkType = {
@@ -18,8 +19,35 @@ export const Link: FC<LinkType> = (link) => {
 export const Search = () => {
   return (
     <div className={styles.search}>
-      <SeacrhIcon />
+      <SeacrhIcon className={styles.icon} />
       <input type="text" placeholder="Search Art Work / Creator" />
+    </div>
+  );
+};
+
+type BurgerMenuProps = {
+  links: { text: string; href: string }[];
+};
+
+export const BurgerMenu: FC<BurgerMenuProps> = ({ links }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(isOpen);
+  };
+
+  return (
+    <div className={styles.burger}>
+      <button onClick={toggleMenu} className={styles.toggle}>
+        <MenuIcon className={styles.icon} />
+      </button>
+      {isOpen && (
+        <div className={styles.menu}>
+          {links.map((link) => (
+            <Link text={link.text} href={link.href} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/widgets/navbar/navbar.widget.tsx
+++ src/widgets/navbar/navbar.widget.tsx
@@ -1,8 +1,13 @@
 import Logo from '@shared/icons/wave.svg';
 import { Button } from '@shared/button';
 import { ButtonSchema } from '@shared/constants/constants';
-import { Link, Search } from './components/navbar.components';
-import styles from './navbar.module.scss';
+import {
+  BurgerMenu,
+  Link,
+  Search,
+} from '@widgets/navbar/components/navbar.components';
+import styles from '@widgets/navbar/navbar.module.scss';
+import { useEffect, useState } from 'react';
 
 const links = [
   {
@@ -24,14 +29,41 @@ const links = [
 ];
 
 function NavBar() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 375);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 375);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className={styles.navbar}>
-      <Logo />
-      {links.map((link) => {
-        return <Link key={link.text} text={link.text} href={link.href} />;
-      })}
-      <Search />
-      <Button text={'connect wallet'} schema={ButtonSchema.dark} />
+      <div className={`${styles.elements} ${styles.elements_left}`}>
+        <Logo className={styles.logo} />
+        {isMobile ? (
+          <div className={styles.title}>DiveSea</div>
+        ) : (
+          links.map((link) => {
+            return <Link key={link.text} text={link.text} href={link.href} />;
+          })
+        )}
+      </div>
+      <div className={`${styles.elements} ${styles.elements_right}`}>
+        {isMobile ? (
+          <BurgerMenu links={links} />
+        ) : (
+          <>
+            <Search />
+            <Button text={'connect wallet'} schema={ButtonSchema.dark} />
+          </>
+        )}
+      </div>
     </div>
   );
 }
